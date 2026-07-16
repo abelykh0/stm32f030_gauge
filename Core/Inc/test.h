89bf79b245d863aad687992b4414f7a204631075
Andrey Belykh <abelykh@yahoo.ca>
2026-07-15 23:15:03 -0400

    Initial

---
+++ Core/Inc/test.h
@@ -0,0 +1,9 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include "stm32f0xx_hal.h"
+
+void EEPROM_Test_Init(void);
+void EEPROM_Test_Run(void);
+
+#endif
---
+++ Core/Src/main.c
@@ -0,0 +1,551 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "DEV_Config.h"
+#include "EPD_2in66.h"
+#include "GUI_Paint.h"
+#include "fonts.h"
+#include "minmea.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define EEPROM_ADDR (0x50 << 1)
+#define NMEA_MAX 128
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi2;
+UART_HandleTypeDef huart1;
+
+/* USER CODE BEGIN PV */
+static UBYTE BlackImage[EPD_2IN66_WIDTH * EPD_2IN66_HEIGHT / 8];
+
+uint8_t gps_rx_byte;
+char gps_line[NMEA_MAX];
+char gps_latest[NMEA_MAX];
+volatile uint8_t gps_ready = 0;
+uint16_t gps_index = 0;
+
+typedef struct
+{
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+
+    uint8_t day;
+    uint8_t month;
+    uint8_t year;
+
+    uint32_t speed_kmh10;
+} GPS_Time_t;
+
+GPS_Time_t gps_time;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+static void MX_SPI2_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1)
+    {
+        uint32_t err = HAL_UART_GetError(huart);
+
+        if (err & HAL_UART_ERROR_ORE)
+        {
+            __HAL_UART_CLEAR_OREFLAG(huart);
+        }
+
+        HAL_UART_Receive_IT(
+            &huart1,
+            &gps_rx_byte,
+            1
+        );
+    }
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1)
+    {
+        char c = gps_rx_byte;
+
+        if (c == '\n')
+        {
+            gps_line[gps_index] = 0;
+
+            // copy completed sentence as "latest"
+            memcpy(
+                gps_latest,
+                gps_line,
+                gps_index + 1
+            );
+
+            gps_ready = 1;
+
+            gps_index = 0;
+        }
+        else
+        {
+            if (gps_index < NMEA_MAX - 1)
+            {
+                gps_line[gps_index++] = c;
+            }
+        }
+
+        // continue receiving
+        HAL_UART_Receive_IT(
+            &huart1,
+            &gps_rx_byte,
+            1
+        );
+    }
+}
+
+void ParseGNRMC(char *nmea)
+{
+    struct minmea_sentence_rmc frame;
+
+    if (minmea_parse_rmc(&frame, nmea))
+    {
+    	gps_time.minute = frame.time.minutes;
+    	gps_time.second = frame.time.seconds;
+
+    	gps_time.day = frame.date.day;
+    	gps_time.month = frame.date.month;
+    	gps_time.year = frame.date.year % 100;
+
+
+    	// apply timezone
+    	int h = frame.time.hours + (-5); // timezone
+
+    	if (h < 0)
+    	{
+    	    h += 24;
+    	    gps_time.day--;       // add month/year handling if needed
+    	}
+    	else if (h >= 24)
+    	{
+    	    h -= 24;
+    	    gps_time.day++;       // add month/year handling if needed
+    	}
+
+    	gps_time.hour = h;
+
+        // km/h * 10
+    	gps_time.speed_kmh10 =
+    	    ((int64_t)frame.speed.value * 1852) /
+    	    frame.speed.scale /
+    	    100;
+    }
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+
+  //EPD_Test();
+  EPD_2IN66_Init();
+  EPD_2IN66_Clear();
+  DEV_Delay_ms(500);
+  Paint_NewImage(BlackImage, EPD_2IN66_WIDTH, EPD_2IN66_HEIGHT, 270, WHITE);
+  EPD_2IN66_Init_Partial();
+
+  HAL_UART_Receive_IT(&huart1, &gps_rx_byte, 1);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  int partial_count = 0;
+
+  while (1)
+  {
+    /* USER CODE END WHILE */
+	  if (gps_ready)
+	      {
+	          char local[NMEA_MAX];
+
+	          __disable_irq();
+
+	          gps_ready = 0;
+	          strcpy(local, gps_latest);
+
+	          __enable_irq();
+
+	          // temporary: show raw NMEA on display
+
+	          if (strncmp(local, "$GNRMC", 6) == 0)
+	          {
+	        	  ParseGNRMC(local);
+
+	        	  partial_count++;
+
+	        	  Paint_ClearWindows(
+					  0,
+					  0,
+					  EPD_2IN66_HEIGHT - 1,
+					  EPD_2IN66_WIDTH - 1,
+					  WHITE
+				  );
+
+				  char display[32];
+
+				  sprintf(
+				      display,
+				      "%02d:%02d",
+				      gps_time.hour,
+				      gps_time.minute
+				  );
+
+
+				  Paint_Clear(WHITE);
+
+				  Paint_DrawString_EN(
+				      10,
+				      120,
+				      display,
+				      &seg7_font_small,
+				      BLACK,
+				      WHITE
+				  );
+
+
+				  sprintf(
+				      display,
+				      "%02d/%02d/20%02d",
+				      gps_time.day,
+				      gps_time.month,
+				      gps_time.year
+				  );
+
+				  Paint_DrawString_EN(
+				      110,
+				      120,
+				      display,
+				      &seg7_font_small,
+				      BLACK,
+				      WHITE
+				  );
+
+				  // Speed
+				  snprintf(
+				      display,
+				      sizeof(display),
+				      "%lu.%lu",
+				      gps_time.speed_kmh10 / 10,
+				      gps_time.speed_kmh10 % 10
+				  );
+
+
+				  Paint_DrawString_EN(
+				      22,
+				      8,
+				      display,
+				      &seg7_font_small,
+				      BLACK,
+				      WHITE
+				  );
+
+				  //Paint_DrawString_EN(10, 20, local, &Font12, BLACK, WHITE);
+
+	        	  if (partial_count >= 60)
+	        	  {
+	        	      partial_count = 0;
+
+	        	      EPD_2IN66_Init();
+	        	      EPD_2IN66_Display(BlackImage);
+	        	      EPD_2IN66_Init_Partial();
+	        	  }
+	        	  else
+	        	  {
+					  EPD_2IN66_Display(BlackImage);
+	        	  }
+	          }
+	          //HAL_Delay(1000);
+	      }
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL6;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 7;
+  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi2.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, RST_Pin|DC_Pin|SPI_CS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PA0 PA1 PA2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : RST_Pin DC_Pin SPI_CS_Pin */
+  GPIO_InitStruct.Pin = RST_Pin|DC_Pin|SPI_CS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA9 PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : BUSY_Pin */
+  GPIO_InitStruct.Pin = BUSY_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(BUSY_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
